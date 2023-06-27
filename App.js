957b9f28dd6341228fc3412f51502e5dee606efc
manan.taneja <manan.taneja@flipkart.com>
2023-06-27 17:28:40 +0530

    fix: dropdown

--- App.js
+++ App.js
@@ -3,21 +3,27 @@ import React, { useState } from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function App() {
-  const [selectedValue, setSelectedValue] = useState(null);
+  const [value, setValue] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [items, setItems] = useState([
+    { label: 'Bangalore', value: 'bangalore' },
+    { label: 'Chennai', value: 'chennai' }
+    { label: 'Mumbai', value: 'Mumbai' }
+  ]);
 
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <DropDownPicker
-          items={[
-            { label: 'Option 1', value: 'option1' },
-            { label: 'Option 2', value: 'option2' },
-            { label: 'Option 3', value: 'option3' },
-          ]}
-          defaultValue={selectedValue}
+          items={items}
+          setItems={setItems}
           placeholder="Choose a city"
           containerStyle={{ height: 40 }}
-          onChangeItem={(item) => setSelectedValue(item.value)}
+          onChangeItem={(item) => setValue(item.value)}
+          open={open}
+          setOpen={setOpen}
+          value={value}
+          setValue={setValue}
         />
       </View>
     </SafeAreaView>
